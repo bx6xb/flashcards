1bd88f760462b2286411ea765e07256131f280a4
Yan Yu <bx6xbChosen1@gmail.com>
2024-06-12 19:38:51 +0600

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { App } from './App.tsx'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
