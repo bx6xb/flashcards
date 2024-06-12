ece305473212d3bf3999b12fdc0aae0f340f85ec
Yan Yu <bx6xbChosen1@gmail.com>
2024-06-12 20:00:51 +0600

    add polymorphic button

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
+import { App } from '@/App'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import './styles/index.scss'
