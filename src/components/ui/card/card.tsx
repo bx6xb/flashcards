acebae93a8c4a37451deb7496aeb5a451a5da7a2
Yan Turnt <bx6xbChosen1@gmail.com>
2024-09-21 12:47:34 +0500

    - fixed card class name override

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -4,5 +4,7 @@ import s from './card.module.scss'
 export type CardProps = ComponentPropsWithoutRef<'div'>
 
 export const Card = (props: CardProps) => {
-  return <div className={`${s.card} ${props.className}`} {...props} />
+  const { className, ...rest } = props
+
+  return <div className={`${s.card} ${className || ''}`} {...rest} />
 }
