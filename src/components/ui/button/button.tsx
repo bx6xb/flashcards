0c70e52ce5d01ecaf701cf312c318509b50539e7
Yan Turnt <bx6xbChosen1@gmail.com>
2024-09-23 22:31:11 +0500

    - added icon property in button component and its display

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,11 +1,16 @@
 import { ComponentPropsWithoutRef, ElementType } from 'react'
 import s from './button.module.scss'
 import { Typography } from '../typography'
+import { Icon } from '../icon'
 
 export type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
   variant?: 'primary' | 'secondary'
   fullWidth?: boolean
+  icon?: {
+    id: string
+    position?: 'left' | 'right'
+  }
 } & ComponentPropsWithoutRef<T>
 
 export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
@@ -15,15 +20,29 @@ export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) 
     className = '',
     as: Component = 'button',
     children,
+    icon,
     ...rest
   } = props
+
+  const iconId = icon?.id
+  const position = icon?.position || 'left'
+
+  let IconComponent: ReturnType<typeof Icon> | undefined
+
+  if (iconId) {
+    IconComponent = (
+      <Icon id={iconId} width={16} height={16} viewBox="0 0 22 22" className={s.icon} />
+    )
+  }
 
   return (
     <Component
       className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
       {...rest}
     >
-      <Typography variant="body-2">{children}</Typography>
+      {icon && position === 'left' && IconComponent}
+      <Typography variant="subtitle-2">{children}</Typography>
+      {icon && position === 'right' && IconComponent}
     </Component>
   )
 }
