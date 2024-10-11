109e36e74c833611da1f12674d3b108f18138571
Yan Turnt <bx6xbChosen1@gmail.com>
2024-10-11 19:55:05 +0500

    added routing, router and rtk

---
+++ src/router.tsx
@@ -0,0 +1,40 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  RouteObject,
+  Outlet,
+  Navigate,
+} from 'react-router-dom'
+import { DecksPage } from './pages/decks.page'
+
+const publicRoutes: RouteObject[] = [
+  {
+    path: '/login',
+    element: <div>login</div>,
+  },
+]
+
+const privateRoutes: RouteObject[] = [
+  {
+    path: '/',
+    element: <DecksPage />,
+  },
+]
+
+const router = createBrowserRouter([
+  {
+    element: <PrivateRoutes />,
+    children: privateRoutes,
+  },
+  ...publicRoutes,
+])
+
+export function Router() {
+  return <RouterProvider router={router} />
+}
+
+function PrivateRoutes() {
+  const isAuthenticated = true
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
+}
