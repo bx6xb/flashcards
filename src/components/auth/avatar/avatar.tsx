e5dec23e21aed39843ccdd1c1d491c987e104460
Yan Turnt <bx6xbChosen1@gmail.com>
2024-10-12 21:18:10 +0500

    added ref support for avatar component

--- src/components/auth/avatar/avatar.tsx
+++ src/components/auth/avatar/avatar.tsx
@@ -1,9 +1,10 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithRef, forwardRef } from 'react'
 import s from './avatar.module.scss'
 
-type AvatarProps = Omit<ComponentPropsWithoutRef<'img'>, 'alt'>
+type AvatarProps = Omit<ComponentPropsWithRef<'img'>, 'alt'>
 
-export const Avatar = (props: AvatarProps) => {
+export const Avatar = forwardRef<HTMLImageElement, AvatarProps>((props, ref) => {
   const { className = '', ...rest } = props
-  return <img alt="avatar" className={`${s.avatar} ${className}`} {...rest} />
-}
+
+  return <img alt="avatar" className={`${s.avatar} ${className}`} ref={ref} {...rest} />
+})
