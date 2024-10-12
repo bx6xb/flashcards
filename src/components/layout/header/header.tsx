4c3e4bd8233f744421ab5d1be088555f3d7c5db1
Yan Turnt <bx6xbChosen1@gmail.com>
2024-10-12 21:18:56 +0500

    added header component

---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,41 @@
+import s from './header.module.scss'
+import { Icon } from '../../ui/icon'
+import { Dropdown } from '@/components/ui/dropdown'
+import { Avatar } from '@/components/auth/avatar'
+import defaultAvatar from '../../../assets/defaultAvatar.png'
+import { Typography } from '@/components/ui/typography'
+
+export const Header = () => {
+  const avatar = <Avatar src={defaultAvatar} />
+  const username = 'Zenow'
+  const email = 'zenow@gmail.com'
+
+  const options = [
+    <div className={s.option}>
+      {avatar}
+      <div>
+        <Typography variant="subtitle-2">{username}</Typography>
+        <Typography variant="caption">{email}</Typography>
+      </div>
+    </div>,
+    <div className={s.option}>
+      <Icon id="person-outline" width={16} height={16} viewBox="0 0 22 22" />
+      My Profile
+    </div>,
+    <div className={s.option}>
+      <Icon id="log-out" width={16} height={16} viewBox="0 0 22 22" />
+      Sign Out
+    </div>,
+  ]
+
+  return (
+    <header className={s.header}>
+      <Icon id="logo" width={157} height={36} viewBox="0 0 157 36" />
+
+      <div>
+        <Typography variant="subtitle-1">{username}</Typography>
+        <Dropdown iconButton={avatar} options={options} />
+      </div>
+    </header>
+  )
+}
