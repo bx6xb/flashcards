dc80e9d0d50f3abf8c5ff5f4b188318714031e80
Yan Turnt <bx6xbChosen1@gmail.com>
2024-10-12 21:51:02 +0500

    added header and container in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Router } from '@/router'
 import { Provider } from 'react-redux'
 import { store } from './services/store'
+import { Header } from './components/layout/header'
+import { Container } from './components/ui/container'
 
 export function App() {
   return (
     <Provider store={store}>
-      <Router />
+      <Header />
+      <Container>
+        <Router />
+      </Container>
     </Provider>
   )
 }
