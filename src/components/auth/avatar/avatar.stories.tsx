88f9388a74d569514f0321444929d26bcebeaf04
Yan Turnt <bx6xbChosen1@gmail.com>
2024-10-13 09:55:51 +0500

    added default avatar photo in avatar stories

--- src/components/auth/avatar/avatar.stories.tsx
+++ src/components/auth/avatar/avatar.stories.tsx
@@ -1,10 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Avatar } from './avatar'
+import defaultAvatar from '../../../assets/defaultAvatar.png'
 
 const meta = {
   title: 'Auth/Avatar',
   component: Avatar,
   tags: ['autodocs'],
+  args: {
+    src: defaultAvatar,
+  },
 } satisfies Meta<typeof Avatar>
 
 export default meta
