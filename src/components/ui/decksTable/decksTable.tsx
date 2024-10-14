d21bce12b0e5b6f767d2b22c4a19b360b9b2f57e
Yan Turnt <bx6xbChosen1@gmail.com>
2024-10-14 18:41:02 +0500

    renamed tableHeadValue to tableHead and added sorting logic and ux for this

--- src/components/ui/decksTable/decksTable.tsx
+++ src/components/ui/decksTable/decksTable.tsx
@@ -1,27 +1,19 @@
 import { Deck } from './deck/deck'
 import s from './decksTable.module.scss'
 import { DeckType } from '@/services/decks/decks.types'
-import { TableHeadValue } from './tableHeadValue/tableHeadValue'
+import { TableHead } from './tableHead/tableHead'
 
 type DecksTableProps = {
   decks: DeckType[]
   className?: string
 }
 
-const theadValues = ['Name', 'Cards', 'Last Updated', 'Created By']
-
 export const DecksTable = ({ decks, className = '' }: DecksTableProps) => {
-  const mappedTheadValues = theadValues.map(v => <TableHeadValue key={v} value={v} />)
   const mappedDecks = decks.map(deck => <Deck key={deck.id} deck={deck} />)
 
   return (
     <table className={`${s.decksTable} ${className}`}>
-      <thead>
-        <tr>
-          {mappedTheadValues}
-          <th />
-        </tr>
-      </thead>
+      <TableHead />
       <tbody>{mappedDecks}</tbody>
     </table>
   )
---
+++ src/components/ui/decksTable/tableHead/tableHead.tsx
@@ -0,0 +1,109 @@
+import { useAppDispatch, useAppSelector } from '@/services/store'
+import { Typography } from '../../typography'
+import { setProperty } from '@/services/stateSlices/decksPageSlice'
+import s from './tableHead.module.scss'
+import { Icon } from '../../icon'
+
+type TableHeadValue =
+  | {
+      value: string
+      trackingCategory: string
+      onClick: () => void
+    }
+  | string
+
+export const TableHead = () => {
+  const orderBy = useAppSelector(state => state.decksPage.orderBy)
+  const dispatch = useAppDispatch()
+
+  const theadValues: TableHeadValue[] = [
+    {
+      value: 'Name',
+      trackingCategory: 'name',
+      onClick() {
+        dispatch(
+          setProperty({
+            property: 'orderBy',
+            value: orderBy === 'null' ? 'name-asc' : orderBy === 'name-asc' ? 'name-desc' : 'null',
+          })
+        )
+      },
+    },
+    {
+      value: 'Cards',
+      trackingCategory: 'cardsCount',
+      onClick() {
+        dispatch(
+          setProperty({
+            property: 'orderBy',
+            value:
+              orderBy === 'null'
+                ? 'cardsCount-asc'
+                : orderBy === 'cardsCount-asc'
+                ? 'cardsCount-desc'
+                : 'null',
+          })
+        )
+      },
+    },
+    {
+      value: 'Last Updated',
+      trackingCategory: 'updated',
+      onClick() {
+        dispatch(
+          setProperty({
+            property: 'orderBy',
+            value:
+              orderBy === 'null'
+                ? 'updated-asc'
+                : orderBy === 'updated-asc'
+                ? 'updated-desc'
+                : 'null',
+          })
+        )
+      },
+    },
+    'Created By',
+  ]
+
+  const mappedTableHeadValues = theadValues.map(v => {
+    if (typeof v === 'string') {
+      return (
+        <th key={v}>
+          <Typography variant="subtitle-2">{v}</Typography>
+        </th>
+      )
+    }
+
+    const { value, trackingCategory, onClick } = v
+
+    const [orderByName, orderByValue] = orderBy.split('-')
+    const shouldAddIcon = orderByName === trackingCategory
+
+    return (
+      <th key={value} className={s.clickable}>
+        <Typography variant="subtitle-2" onClick={onClick}>
+          {value}
+          {shouldAddIcon && (
+            <Icon
+              data-sort-by={orderByValue}
+              id="arrow-ios-Up"
+              width={12}
+              height={12}
+              viewBox="0 0 22 22"
+            />
+          )}
+        </Typography>
+      </th>
+    )
+  })
+
+  return (
+    <thead>
+      <tr>
+        {mappedTableHeadValues}
+        <th />
+      </tr>
+    </thead>
+  )
+}
