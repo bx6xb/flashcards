cd28f4f53c06d931ba262cab4893e3389787dd79
Yan Turnt <bx6xbChosen1@gmail.com>
2024-10-15 17:55:39 +0500

    added user authorization and app initialiaztion logic in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 import { Router } from '@/router'
-import { Provider } from 'react-redux'
-import { store } from './services/store'
+import { useAppDispatch } from './services/store'
 import { Header } from './components/layout/header'
 import { Container } from './components/ui/container'
+import { useMeQuery } from './services/flashcardsApi'
+import { setAppProperty } from './services/app/appSlice'
+import { useEffect } from 'react'
 
 export function App() {
+  const dispatch = useAppDispatch()
+
+  const { data, isLoading, error } = useMeQuery()
+
+  useEffect(() => {
+    if (data) {
+      dispatch(setAppProperty({ property: 'isAppInitialized', value: true }))
+      dispatch(setAppProperty({ property: 'isUserAuthorized', value: data.isEmailVerified }))
+    }
+  }, [data])
+
+  if (isLoading) {
+    return 'ZenOw...'
+  }
+
+  if (error) {
+    console.error(error)
+  }
+
   return (
-    <Provider store={store}>
+    <>
       <Header />
       <Container className="appContentWrapper">
         <Router />
       </Container>
-    </Provider>
+    </>
   )
 }
