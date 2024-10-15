17748679a131df4d923b99e469eba061fc7b01e5
Yan Turnt <bx6xbChosen1@gmail.com>
2024-10-15 17:40:37 +0500

    moved redux provider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from '@/App'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import './styles/index.scss'
+import { Provider } from 'react-redux'
+import { store } from './services/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 )
